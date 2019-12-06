92ad0184c8381f52666586a201bad770f6793fa6
Evgeni Gordeev <egordeev@conserviscorp.com>
2019-12-06 10:20:54 -0600

    https://adventofcode.com/2019/day/5 part 2 - solved, but the code is pile of garbage.

--- 2019/2019-day-5.py
+++ 2019/2019-day-5.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 
-
 def _input(location='input/2019-day-5.txt'):
     cwd = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(cwd, location)) as f:
@@ -33,9 +32,9 @@ def param_mode(opcode):
 
 def get_index(int_codes: list, index: int, mode: int):
     if mode == 0:  # positional
-        return int_codes[index]
+        return int_codes[int_codes[index]]
     elif mode == 1:  # immediate
-        return index
+        return int_codes[index]
     else:
         raise ValueError
 
@@ -43,17 +42,14 @@ def get_index(int_codes: list, index: int, mode: int):
 def intcode(int_codes, _in=1):
     instruction_pointer = 0
     output = []
-    while instruction_pointer < len(int_codes):
+    while int_codes[instruction_pointer] != 99:
         code_with_params = param_mode(int_codes[instruction_pointer])
         opcode = code_with_params[0]
         param_modes = code_with_params[1]
         if opcode in [1, 2]:
-            param1 = get_index(int_codes, instruction_pointer + 1, param_modes[0])
-            param2 = get_index(int_codes, instruction_pointer + 2, param_modes[1])
-            param3 = get_index(int_codes, instruction_pointer + 3, param_modes[2])
-
-            a = int_codes[param1]
-            b = int_codes[param2]
+            a = get_index(int_codes, instruction_pointer + 1, param_modes[0])
+            b = get_index(int_codes, instruction_pointer + 2, param_modes[1])
+            c = int_codes[instruction_pointer + 3]
 
             if opcode == 1:
                 new_val = a + b  # add
@@ -62,50 +58,52 @@ def intcode(int_codes, _in=1):
             else:
                 assert False
 
-            int_codes[param3] = new_val  # store value
+            int_codes[c] = new_val  # store value
             instruction_pointer += 4
-        elif opcode in [3, 4]:
-            new_index = int_codes[instruction_pointer + 1]
-            if opcode == 3:
-                int_codes[new_index] = _in  # store value
-            elif opcode == 4:
-                _out = int_codes[new_index]
-                output.append(_out)
-                # print(f"Output is {_out}")
+        elif opcode == 3:
+            a = int_codes[instruction_pointer + 1]
+            int_codes[a] = _in  # store value
+            instruction_pointer += 2
+        elif opcode == 4:
+            a = int_codes[instruction_pointer + 1]
+            # todo: pile of garbage
+            if a < len(int_codes):
+                _out = int_codes[a]
             else:
-                assert False
+                _out = a
+            output.append(_out)
             instruction_pointer += 2
         elif opcode == 5:  # jump-if-true
-            param1 = get_index(int_codes, instruction_pointer + 1, 0) # using param_modes - index of bounds
-            param2 = get_index(int_codes, instruction_pointer + 2, 0) # using param_modes - index of bounds
-            if param1 != 0:
-                instruction_pointer = param2
+            a = get_index(int_codes, instruction_pointer + 1, param_modes[0])
+            b = get_index(int_codes, instruction_pointer + 2, param_modes[1])
+            if a != 0:
+                instruction_pointer = b
             else:
                 instruction_pointer += 3
         elif opcode == 6:  # jump-if-false
-            param1 = get_index(int_codes, instruction_pointer + 1, 0) # using param_modes - index of bounds
-            param2 = get_index(int_codes, instruction_pointer + 2, 0) # using param_modes - index of bounds
-            if param1 == 0:
-                instruction_pointer = param2
+            a = get_index(int_codes, instruction_pointer + 1, param_modes[0])
+            b = get_index(int_codes, instruction_pointer + 2, param_modes[1])
+            if a == 0:
+                instruction_pointer = b
             else:
                 instruction_pointer += 3
         elif opcode == 7:  # less than
-            param1 = get_index(int_codes, instruction_pointer + 1, 0) # using param_modes - index of bounds
-            param2 = get_index(int_codes, instruction_pointer + 2, 0) # using param_modes - index of bounds
-            param3 = get_index(int_codes, instruction_pointer + 3, 0) # using param_modes - index of bounds
-            if param1 < param2:
-                int_codes[param3] = 1
+            a = get_index(int_codes, instruction_pointer + 1, param_modes[0])
+            b = get_index(int_codes, instruction_pointer + 2, param_modes[1])
+            c = int_codes[instruction_pointer + 3]
+            if a < b:
+                int_codes[c] = 1
             else:
-                int_codes[param3] = 0
+                int_codes[c] = 0
             instruction_pointer += 4
         elif opcode == 8:  # equals
-            param1 = get_index(int_codes, instruction_pointer + 1, 0)
-            param2 = get_index(int_codes, instruction_pointer + 2, 0)
-            param3 = get_index(int_codes, instruction_pointer + 3, 0)
-            if param1 == param2:
-                int_codes[param3] = 1
+            a = get_index(int_codes, instruction_pointer + 1, param_modes[0])
+            b = get_index(int_codes, instruction_pointer + 2, param_modes[1])
+            c = int_codes[instruction_pointer + 3]
+            if a == b:
+                int_codes[c] = 1
             else:
-                int_codes[param3] = 0
+                int_codes[c] = 0
             instruction_pointer += 4
         elif opcode == 99:  # halt
             break
@@ -122,26 +120,22 @@ def test_part1():
     result1 = intcode([1002, 4, 3, 4, 33])
     assert result1[0] == [1002, 4, 3, 4, 99]
     assert result1[1] == []
-    result2 = intcode([3, 0, 4, 0, 99])
-    assert result2[0] == [1, 0, 4, 0, 99]
-    assert result2[1] == [1]
 
 
 def test_part2():
     result11 = intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 1)
     assert result11[1] == [1]
     result12 = intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 0)
-    #  assert result2[1] == [0] # todo: this fails
-    result2 = intcode([3,9,8,9,10,9,4,9,99,-1,8], 7)
+    assert result12[1] == [0]
     test_codes = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
                   1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
                   999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
     result31 = intcode(test_codes, 7)
-    # assert result31[1] == [999] # todo: this fails
+    assert result31[1] == [999]
     result32 = intcode(test_codes, 8)
     assert result32[1] == [1000]
     result33 = intcode(test_codes, 9)
-    # assert result33[1] == [1001] # todo: this fails
+    assert result33[1] == [1001]
 
 
 if __name__ == "__main__":
@@ -150,6 +144,6 @@ if __name__ == "__main__":
     part1 = asteroids(_input())
     output1 = part1[1]
     print(f"https://adventofcode.com/2019/day/5, answer is {output1[-1]}")
-    # part2 = asteroids(_input(), 5)
-    # print(f"https://adventofcode.com/2019/day/5#part2, answer is {100 * noun + verb}")
+    part2 = asteroids(_input(), 5)
+    print(f"https://adventofcode.com/2019/day/5#part2, answer is {part2[1][-1]}")
     exit(0)
