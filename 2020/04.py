9db1d7b4a567047312e751846333fc9ea3df8e74
Evgeni Gordeev <egordeev@conserviscorp.com>
2020-12-04 13:40:53 -0600

    2020 day 4 - polish

--- 2020/04.py
+++ 2020/04.py
@@ -95,24 +95,13 @@ part_one = count_valid_passports(lines, fields_exist)
 assert part_one == 2
 
 # test 2
-test_input = """
-pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
-hcl:#623a2f
-
-eyr:2029 ecl:blu cid:129 byr:1989
-iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-
-hcl:#888785
-hgt:164cm byr:2001 iyr:2015 cid:88
-pid:545766238 ecl:hzl
-eyr:2022
-
+test_input += """
 iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
 """
 lines = parse(test_input)
 
 part_two = count_valid_passports(lines, validate_fields)
-assert part_two == 4
+assert part_two == 3
 # SOLUTION
 passes = parse(read_data())
 # part#1
