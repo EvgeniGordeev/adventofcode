21285aa60c00c7fe247c45c8761a0babb79baabb
Evgeni Gordeev <egordeev@conserviscorp.com>
2020-12-19 19:39:33 -0600

    2020 day 15 - unoptimized

--- 2020/15.py
+++ 2020/15.py
@@ -13,7 +13,7 @@ def read_input() -> str:
 
 
 # MAIN FUNCTIONS
-def part1(nums: list, end: int = 2020) -> int:
+def last_number_spoken(nums: list, end: int = 2020) -> int:
     size, memo = len(nums), dict()
 
     debug_sequence = []
@@ -49,12 +49,12 @@ def test():
     given = parser("""
 0,3,6
 """)
-    assert part1(given, 2020) == 436
-    assert part1([1, 3, 2], 2020) == 1
-    assert part1([2, 1, 3], 2020) == 10
-    assert part1([2, 3, 1], 2020) == 78
-    assert part1([3, 2, 1], 2020) == 438
-    assert part1([3, 1, 2], 2020) == 1836
+    assert last_number_spoken(given, 2020) == 436
+    assert last_number_spoken([1, 3, 2], 2020) == 1
+    assert last_number_spoken([2, 1, 3], 2020) == 10
+    assert last_number_spoken([2, 3, 1], 2020) == 78
+    assert last_number_spoken([3, 2, 1], 2020) == 438
+    assert last_number_spoken([3, 1, 2], 2020) == 1836
     # part 2
     # assert part1([0, 3, 6], 30000000) == 175594
 
@@ -65,11 +65,11 @@ if __name__ == '__main__':
     assert test()
     nums = parser(read_input())
     # ONE #1
-    part_1 = part1(nums)
+    part_1 = last_number_spoken(nums)
     print(part_1)
     assert part_1 == 234
     # # TWO #2
-    part_2 = part1(nums, 30000000)
+    part_2 = last_number_spoken(nums, 30000000)
     print(part_2)
     assert part_2 == 8984
 
